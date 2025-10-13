40710a82fd40db9068630f8991626f89b82682a0
PatricioSN <patriciosppb@gmail.com>
2025-10-13 12:06:29 -0300

    fixed screen_meneger, it no longer crashes if it can't find the image.

--- screen_meneger.py
+++ screen_meneger.py
@@ -1,12 +1,17 @@
 import logging
 import cv2
 import pyautogui
+import pyscreeze
+
 
 class ScreenMeneger:
 
     @staticmethod
     def get_all_matches_by_image(image_to_search: str):
         try:
-            return pyautogui.locateAllOnScreen(image=image_to_search, grayscale=True, confidence=0.9)
+            return list(pyautogui.locateAllOnScreen(image=image_to_search, grayscale=True, confidence=0.9))
+        except pyscreeze.ImageNotFoundException:
+            return []
         except Exception as e:
             logging.info(f"Error: {e}")
+            return []
