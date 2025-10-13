8bb1a01f59e3ebddcb8e07b27ca9d11f9b1ed72f
PatricioSN <patriciosppb@gmail.com>
2025-10-13 12:31:06 -0300

    now method 'is_app_focused'.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from screen_meneger import ScreenMeneger
 
 def run():
     while True:
-        coordinates = ScreenMeneger.get_all_matches_by_image(image_to_search=r'.\foxy.png')
-        for coordinate in coordinates:
-            print(coordinate)
-
+        # coordinates = ScreenMeneger.get_all_matches_by_image(image_to_search=r'.\foxy.png')
+        # for coordinate in coordinates:
+        #     print(coordinate)
+        print(ScreenMeneger.is_app_focused(app_title_name="instagram"))
 run()
--- screen_meneger.py
+++ screen_meneger.py
@@ -2,10 +2,12 @@ import logging
 import cv2
 import pyautogui
 import pyscreeze
+import win32gui
 
 
 class ScreenMeneger:
 
+    # The method tries to find the image on the screen and returns the coordinates of the found image.
     @staticmethod
     def get_all_matches_by_image(image_to_search: str):
         try:
@@ -15,3 +17,10 @@ class ScreenMeneger:
         except Exception as e:
             logging.info(f"Error: {e}")
             return []
+
+
+    # The method checks if the application is focused.
+    @staticmethod
+    def is_app_focused(app_title_name: str):
+        active_window_title = win32gui.GetWindowText(win32gui.GetForegroundWindow()).lower()
+        return app_title_name in active_window_title
